d5a6db839aa81d1dfb75ce2c60474dc6aff1b646
FOSTER <Charlotte.FOSTER@education.gov.uk>
2023-01-20 15:07:22 +0000

    save data for tasks

---
+++ R/R_workshop_solution.R
@@ -0,0 +1,11 @@
+install.packages("dplyr")
+
+library(readr)
+library(dplyr)
+
+some_student_results <- read_csv("data/some_student_results.csv")
+View(some_student_results)
+
+some_student_results <- some_student_results %>%
+  group_by(school, sex, age, address) %>%
+  summarise(G1_mean = mean(G1), G2_mean = mean(G2), G3_mean = mean(G3))
