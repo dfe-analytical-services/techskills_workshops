c787e09838b599640da4426d33b378d6f50f32c0
Lara Edington <lara.edington@education.gov.uk>
2026-08-10 16:42:02 +0100

    Add schools data manipulation and join to pupils

--- Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline/code/data_manipulation.R
+++ Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline/code/data_manipulation.R
@@ -9,19 +9,16 @@ pupils_aut <- pupils_aut_raw %>%
   select(pupil_id, 
          gender, 
          school_urn, 
-         metadata_json)
-
-## Separate metadata_json into multiple columns
-pupils_aut %<>%
+         metadata_json) %>%
+  ## Separate metadata_json into multiple columns and keep those we want
   separate(metadata_json, 
            into = c("address", "contact", "blank_1", "sen_fsm", "blank_2"), 
            sep = "}") %>%
-  ## Remove columns we don't need
   select(-address,
          -contact, 
          -blank_1, 
          -blank_2) %>%
-  # Create clean SEN and FSM status columns
+  # Create separate SEN and FSM status columns and tidy them
   mutate(sen_fsm = str_extract(sen_fsm, ".*\\b(?:true|false)\\b"),
          sen_fsm = str_replace(sen_fsm, ', "sen_status": ', '')) %>%
   separate(sen_fsm, into = c("sen_status", "fsm_status"), sep = ",") %>%
@@ -34,12 +31,23 @@ pupils_aut %<>%
 
 ## Select the columns we want
 schools_aut <- schools_aut_raw %>%
-  select(-school_name)
-
-## Separate metadata_json into multiple columns
-schools_aut %<>%
+  select(-school_name, 
+         -status) %>%
+  ## Separate metadata_json into multiple columns and keep those we want
   separate(metadata_json, 
            into = c("ofsted_rating", "last_inspection", "pupil_premium_pct"), 
            sep = ",") %>%
-  mutate(ofsted_rating = str_replace(ofsted_rating, '[{]"ofsted_rating": "', ''),
-         ofsted_rating = str_replace(ofsted_rating, '"', ''))
+  select(-last_inspection) %>%
+  ## Tidy remaining columns
+  mutate(city = str_to_title(city),
+         ofsted_rating = str_replace(ofsted_rating, '[{]"ofsted_rating": "', ''),
+         ofsted_rating = str_replace(ofsted_rating, '"', ''),
+         pupil_premium_pct = str_replace(pupil_premium_pct, '"pupil_premium_pct": ', ''),
+         pupil_premium_pct = as.numeric(str_replace(pupil_premium_pct, '[}]', ''))) %>%
+  ## De-duplicate entries
+  unique()
+
+# Join pupils and schools datasets ----
+
+pupils_schools_aut <- pupils_aut %>%
+  left_join(schools_aut, by = "school_urn")
