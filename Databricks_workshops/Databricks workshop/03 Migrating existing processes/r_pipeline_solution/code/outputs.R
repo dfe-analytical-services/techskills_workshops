9de323a19b80731a4dfd36036287114bec93e954
Lara Garbett <lara.garbett@education.gov.uk>
2026-08-26 12:08:33 +0100

    Change R pipeline solution to write outputs to Databricks catalog

--- Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline_solution/code/outputs.R
+++ Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline_solution/code/outputs.R
@@ -12,19 +12,32 @@ avg_pupil_premium_by_ofsted <- pupils_schools_aut %>%
     median_pupil_premium_pct = median(pupil_premium_pct, na.rm = TRUE),
     .groups = "drop"
   ) %>%
-  arrange(desc(pupils))
+  arrange(desc(pupils)) %>%
+  filter(!is.na(ofsted_rating))
 
-# Save to outputs folder ----
-write.csv(
-  avg_pupil_premium_by_ofsted,
-  file = "Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline_solution/outputs/avg_pupil_premium_by_ofsted_migrated.csv",
-  row.names = FALSE
-)
+# Save to your team's Databricks catalog in a schema called <your_name> ----
+
+## Specify your team's catalog
+## NOTE: You will need to change this to your own team's catalog name!
+DBI::dbExecute(con, "USE CATALOG catalog_40_copper_statistics_services;")
+
+## Create a schema called <your_name> if it doesn't already exist in that schema and specify as the schema to use
+## NOTE: Change "lgarbett" to your own name!
+DBI::dbExecute(con, "CREATE SCHEMA IF NOT EXISTS lgarbett;")
+DBI::dbExecute(con, "USE lgarbett;")
+
+## Write the output to your schema in the Databricks catalog
+DBI::dbWriteTable(conn = con,
+                  name = "pupil_premium_ofsted",
+                  value = avg_pupil_premium_by_ofsted,
+                  overwrite = TRUE,
+                  append = FALSE,
+                  row.names = FALSE)
 
 # Check migrated output matches that from original pipeline ----
 avg_pupil_premium_by_ofsted_orig <- read.csv("Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline/outputs/avg_pupil_premium_by_ofsted.csv")
-avg_pupil_premium_by_ofsted_migrated <- read.csv("Databricks_workshops/Databricks workshop/03 Migrating existing processes/r_pipeline_solution/outputs/avg_pupil_premium_by_ofsted_migrated.csv")
-if (identical(avg_pupil_premium_by_ofsted_orig, avg_pupil_premium_by_ofsted_migrated)) {
+
+if (identical(avg_pupil_premium_by_ofsted_orig, avg_pupil_premium_by_ofsted)) {
   paste("SUCCESS! Migrated output matches that from original pipeline")
 } else {
   stop("Migrated output does not match that from original pipeline")
